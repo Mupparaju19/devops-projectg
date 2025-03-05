5514c264f7e821b859e5ef3f544af1003b7ddca5
muralimupparaju <muralimupparaju@gmail.com>
2025-03-05 17:03:46 -0500

    added few files

---
+++ src/utils/gameLogic.ts
@@ -0,0 +1,21 @@
+export type Player = "X" | "O" | null;
+
+export const winningCombinations = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [2, 4, 6]
+];
+
+export const checkWinner = (squares: Player[]): Player | null => {
+  for (let combination of winningCombinations) {
+    const [a, b, c] = combination;
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+export const isDraw = (squares: Player[]): boolean => {
+  return squares.every(square => square !== null);
+};
