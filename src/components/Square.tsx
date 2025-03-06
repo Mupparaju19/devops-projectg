684213d02a2d0262f25d0191677fad765730d847
muralimupparaju <muralimupparaju@gmail.com>
2025-03-06 13:47:32 -0500

    updated these file

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -7,11 +7,16 @@ interface SquareProps {
 
 const Square: React.FC<SquareProps> = ({ value, onClick }) => {
   return (
-    <button className={`square ${value === "X" ? "x glow" : value === "O" ? "o glow" : ""}`} onClick={onClick}>
+    <button
+      className="w-20 h-20 flex items-center justify-center border-2 border-gray-300 text-3xl font-bold text-gray-800 
+      bg-gray-50 hover:bg-indigo-200 transition-all rounded-md shadow-md"
+      onClick={onClick}
+    >
       {value}
     </button>
   );
 };
 
 export default Square;
+
 
